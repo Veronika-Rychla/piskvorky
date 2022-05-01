2d0e37a6ce97f4ba54722f71a92d5350892946d0
Veronika Rychlá <veronika.rychla108@gmail.com>
2022-05-01 20:19:06 +0200

    Výherní pozice

--- index.js
+++ index.js
@@ -14,9 +14,97 @@ const handleClick = (event) => {
     event.target.classList.add('board__field--cross');
   }
   event.target.disabled = true;
-  console.log('funguju');
+
+  const isWinning = isWinningMove(event.target);
+
+  if (isWinning) {
+    const symbol = getSymbol(event.target);
+    if (confirm(`Vyhrál ${symbol}. Spustit novou hru?`) === true) {
+      location.reload();
+    }
+  }
+  console.log(isWinningMove(event.target));
 };
 
 for (let i = 0; i < board.length; i++) {
   board[i].addEventListener('click', handleClick);
 }
+
+const getSymbol = (field) => {
+  if (field.classList.contains('board__field--cross')) {
+    return 'křížek';
+  } else if (field.classList.contains('board__field--circle')) {
+    return 'kolečko';
+  }
+  return undefined;
+};
+
+const boardSize = 10;
+const fields = document.querySelectorAll('.herniplocha button');
+
+const getField = (row, column) => {
+  return fields[row * boardSize + column];
+};
+
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length && field !== fields[fieldIndex]) {
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
